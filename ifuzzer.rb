5541a6f4162f3226b0075ab501b2e2ad662ca572
stillwalkin <info@stillwalkin.com>
2015-05-19 20:31:25 -0400

    shell console menu

---
+++ ifuzzer.rb
@@ -0,0 +1,61 @@
+require 'highline'
+require 'cli-console'
+
+
+class ShellUI
+    private
+    extend CLI::Task
+
+    public
+
+    usage 'Usage: load <adapter>'
+    desc 'Load Adapters'
+    def load(params)
+      puts "loading adapter #{params[0]}..."
+    end
+
+    usage 'Usage: set <var> <val>'
+    desc 'Set module options'
+
+    def set(params)
+        puts "setting #{params[0]} to #{params[1]}..."
+    end
+
+    usage 'Usage: use <fuzzer-name>'
+    desc 'Use a certain fuzzer'
+
+    def use(params)
+        puts "using the intelligentFuzzer..."
+    end
+
+    usage 'Usage: search <string>'
+    desc 'Search for fuzzers, books, and adapters'
+
+    def search(params)
+        puts "searching for #{params[0]}..."
+    end
+
+    usage 'Usage: show <string>'
+    desc 'show fuzzer types, book recipes, and adapters'
+
+    def show(params)
+        puts "showing #{params[0]}..."
+    end
+
+end
+
+io = HighLine.new
+shell = ShellUI.new
+console = CLI::Console.new(io)
+
+console.addCommand('load', shell.method(:load), 'load $adapter')
+console.addCommand('use', shell.method(:use), 'use $fuzzer')
+console.addCommand('search', shell.method(:search), 'search $term')
+console.addCommand('set', shell.method(:set), 'set module options')
+console.addCommand('show', shell.method(:show), 'show books, adapters, and fuzzers')
+console.addHelpCommand('help', 'Help')
+console.addExitCommand('exit', 'Exit from program')
+console.addAlias('quit', 'exit')
+
+console.start("%s> ",[Dir.method(:pwd)])
+
